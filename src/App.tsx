8274489948a49d90038a1614717f08057fa26546
Andy Ansong <andyansong82@gmail.com>
2025-08-10 01:30:50 +0000

    setup shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
+import { Button } from "@/components/ui/button"
 
-const App: React.FC = () => {
+function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold">Welcome to the App</h1>
-      <p className="mt-4 text-lg">This is a simple React application.</p>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
     </div>
   )
 }
