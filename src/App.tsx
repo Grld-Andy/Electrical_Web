3cae62aebe71d69102e84425b8e0dc25fc9de092
Andy Ansong <andyansong82@gmail.com>
2025-08-14 16:15:58 +0000

    design footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Home } from "lucide-react"
 import { Route, Routes } from "react-router-dom"
-import MainLayout from "./components/MainLayout"
+import MainLayout from "./components/Layout/MainLayout"
 import About from "./pages/About"
 import Contactus from "./pages/Contactus"
 import NoPage from "./pages/NoPage"
 import Services from "./pages/Services"
+import Projects from "./pages/Projects"
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path="/" element={<MainLayout />}>
         <Route index element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route path="projects" element={<Projects />} />
         <Route path="contact" element={<Contactus />} />
         <Route path="services" element={<Services />} />
         <Route path="*" element={<NoPage />} />
