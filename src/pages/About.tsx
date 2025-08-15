c016afb5f6330ae5daff2ab7fe00cd860c0b7a65
Nkrumah27 <119161107+Nkrumah27@users.noreply.github.com>
2025-08-15 16:31:11 +0000

    add marin

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -86,7 +86,7 @@ const AboutUs: React.FC = () => {
       </div>
 
       {/* Team Section */}
-      <div className="max-w-6xl mx-auto px-4 py-8 bg-white">
+      <div className=" px-15 py-8 bg-white">
         <h2 className="text-2xl font-bold text-center mb-2">Our Team</h2>
         <p className="text-center text-gray-600 mb-8">
           Meet our professional team members who are ever ready and up to the
